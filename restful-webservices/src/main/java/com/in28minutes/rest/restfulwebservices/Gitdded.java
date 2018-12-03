efe173519bf5b99da2788a043ef08a0e61f0ff8a
nitin <nithin.poosarla@gmail.com>
2018-12-04 03:51:39 +0530

    new class added to project git

---
+++ restful-webservices/src/main/java/com/in28minutes/rest/restfulwebservices/Gitdded.java
@@ -0,0 +1,20 @@
+package com.in28minutes.rest.restfulwebservices;
+
+public class Gitdded 
+{
+	private String gitid;
+	private String gitname;
+	public String getGitname() {
+		return gitname;
+	}
+	public String getGitid() {
+		return gitid;
+	}
+	public void setGitid(String gitid) {
+		this.gitid = gitid;
+	}
+	public void setGitname(String gitname) {
+		this.gitname = gitname;
+	}
+
+}
