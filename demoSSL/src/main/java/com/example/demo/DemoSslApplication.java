b53815d09fb6852835d5a5edc1738b4497fb03b2
nitin <nithin.poosarla@gmail.com>
2018-12-06 13:57:36 +0530

    new project

---
+++ demoSSL/src/main/java/com/example/demo/DemoSslApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSslApplication.class, args);
+	}
+}
